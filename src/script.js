fedae25cf0a5b0a6e9034860046c45c5b73db8b0
James Kearl <james.kearl@gmail.com>
2019-07-23 18:26:04 -0600

    classes created, static garden created, formatted in css

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 /*
-
 garden:
 	id - unique id
 	grid - 2D Arr
@@ -22,21 +21,146 @@ garden:
 	-genTroll()
 	-placeNewCreature()
 	-getTimeInNSeconds()
-
-creature:
-
-setInterval
 */
+const gardenFrame = document.getElementById('whole-garden');
+const userInputs = document.getElementById('user-inputs');
+const gridContent = document.getElementById('grid-content');
 
-const garden = {
-  name: 'sam',
-  id: 34,
-  blah: {
-    cool: true,
-    things: [5, 6, 7, 8]
-  }
-};
+class Garden {
+	constructor(id, age, gnome, trolls, grid, status) {
+		this.id = id;
+		this.age = age;
+		this.gnome = gnome;
+		this.trolls = trolls;
+		this.grid = grid;
+		this.status = status;
+		this.paused = true;
+	}
+	timer() {
 
+	}
+	start() {
+
+	}
+	stop() {
+
+	}
+	pause() {
+
+	}
+	save() {
+
+	}
+	finish () {
+
+	}
+	render () {
+
+	}
+	executeTurn() {
+
+	}
+	handeMove() {
+
+	}
+	genGnome() {
+
+	}
+	genTroll() {
+
+	}
+	placeNewCreature() {
+
+	}
+	getTimeInSeconds() {
+
+	}
+}
+
+class Creature {
+	constructor(coordinates, hunger) {
+		this.coordinates = coordinates;
+		this.hunger = hunger;
+	}
+	getMove() {
+
+	}
+	getTargets(grid) {
+
+	}
+	updateLocation(coordinates) {
+
+	}
+	resetHunger() {
+		hunger = 0;
+	}
+	isStarved() {
+
+	}
+}
+class Gnome extends Creature {
+	constructor(health, symbol = '@', planted) {
+		this.health = health;
+		this.maxHunger = 10;
+		this.name = 'Thelonious';
+		this.symbol = symbol;
+		this.planted = planted;
+	}
+	tryPlant(planted) {
+
+	}
+	getTargets() {
+
+	}
+}
+
+class Troll extends Creature {
+	constructor(symbol = '&') {
+		this.maxHunger = 20;
+		this.name = 'Miles';
+		this.symbol = symbol;
+	}
+}
+let inputGrid = [
+  ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+  ['#', '@', '$', '_', '#', '$', '_', '_', '_', '#'],
+  ['#', '#', '_', '_', '$', '$', '_', '$', '_', '#'],
+  ['#', '_', '$', '&', '$', '_', '_', '$', '_', '#'],
+  ['#', '_', '$', '_', '_', '$', '_', '_', '_', '#'],
+  ['#', '_', '$', '_', '$', '_', '$', '_', '_', '#'],
+  ['#', '_', '$', '_', '$', '_', '$', '_', '_', '#'],
+  ['#', '_', '_', '_', '_', '$', '_', '$', '_', '#'],
+  ['#', '_', '&', '$', '$', '&', '_', '$', '_', '#'],
+  ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
+];
+
+const gridFill = (grid) => {
+	for(let i = 0; i < grid.length; i++) {
+		var newRow = gridContent.appendChild(document.createElement('div'))
+		newRow.id = 'row' + i;
+		newRow.className = 'rows';
+		for(let j = 0; j < grid[i].length; j++) {
+			let newCell = document.createElement('div');
+			newCell.innerHTML = grid[i][j];
+			newRow.appendChild(newCell);
+			console.log(grid[i][j]);
+		}
+
+	}
+}
+
+const currGarden = new Garden(24,3, {}, {}, inputGrid, 'safe');
+
+gridFill(currGarden.grid);
+
+// CLICK NEW GARDEN BUTTON TO REPOPULATE GARDEN
+// const newGarden = () => currGarden = new Garden()
+
+// userInputs.addEventListener('click', function(event) {
+
+// });
+
+/*
 localStorage.setItem('garden', JSON.stringify(garden));
 // console.log();
 const dehydratedGarden = localStorage.getItem('garden');
@@ -52,3 +176,5 @@ const timer = setInterval(() => {
   console.log('hey');
   count++;
 }, 1000);
+
+*/
